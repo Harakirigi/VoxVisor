8c54b90ef99a4831ef3a021495ea31b14ca9d8b2
Harakirigi <nkomyt2@gmail.com>
2025-04-09 21:37:08 +0500

    v0

--- app/static/app.js
+++ app/static/app.js
@@ -1,3 +1,7 @@
+
+var modal = document.getElementById('modal');
+var span = document.getElementsByClassName('close')[0];
+
 async function uploadImageForText() {
     const fileInput = document.getElementById('imageInput');
     const formData = new FormData();
@@ -9,6 +13,18 @@ async function uploadImageForText() {
     });
 
     const data = await response.json();
+
+
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+
     document.getElementById('textResult').innerText = 'Text: ' + data.recognized_text;
 }
 
@@ -21,34 +37,18 @@ async function uploadAudioForTranscription() {
         method: 'POST',
         body: formData
     });
-
     const data = await response.json();
+
+
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+
     document.getElementById('audioResult').innerText = 'Transcription: ' + data.transcription;
 }
-
-
-
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
-  
